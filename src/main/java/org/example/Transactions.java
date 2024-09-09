1841875d33679f566e3a8df421b5698f18c4ab82
Benjamin Munera <benjaminmunera0@gmail.com>
2024-09-09 07:23:02 -0500

    se crean las identidades sobre las cuales se va a trabajar

---
+++ src/main/java/org/example/Transactions.java
@@ -0,0 +1,7 @@
+package org.example;
+import java.util.Scanner;
+
+public class Transactions {
+    Scanner sc= new Scanner(System.in);
+
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,7 @@
+package org.example;
+import java.util.Scanner;
+
+public class User {
+    Scanner sc= new Scanner(System.in);
+
+}
